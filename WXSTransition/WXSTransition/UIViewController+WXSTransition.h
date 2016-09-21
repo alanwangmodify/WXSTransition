9f84f43f7de001d94228653eff54e5607563b46b
AlanWang <alanwangmodify@126.com>
2016-09-21 20:00:55 +0800

    fix bug on iPhone5

--- WXSTransition/WXSTransition/UIViewController+WXSTransition.h
+++ WXSTransition/WXSTransition/UIViewController+WXSTransition.h
@@ -8,13 +8,7 @@ typedef void(^WXSTransitionBlock)(WXSTransitionProperty *transition);
 
 @interface UIViewController (WXSTransition) <UIViewControllerTransitioningDelegate,UINavigationControllerDelegate>
 
-@property (nonatomic, assign) WXSTransitionAnimationType    wxs_animationType;
-@property (nonatomic, copy  ) WXSTransitionBlock            wxs_callBackTransition;
-@property (nonatomic, assign) BOOL                          wxs_delegateFlag;
-@property (nonatomic, assign) BOOL                          wxs_addTransitionFlag;
 
-@property (nonatomic, weak  ) id                            wxs_transitioningDelegate;
-@property (nonatomic, weak  ) id                            wxs_tempNavDelegate;
 
 
 - (void)wxs_presentViewController:(UIViewController *)viewControllerToPresent animationType:(WXSTransitionAnimationType )animationType completion:(void (^)(void))completion;
