f63c398d41fb1ce32a8f3bfc61b7aa511fdb3a4b
AlanWang <alanwangmodify@126.com>
2017-01-22 19:04:24 +0800

    xib

---
+++ WXSTransition/XibTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  XibTestViewController.h
+//  WXSTransition
+//
+//  Created by AlanWang on 17/1/22.
+//  Copyright © 2017年 王小树. All rights reserved.
+//
+
+#import "WXSBaseViewController.h"
+
+@interface XibTestViewController : WXSBaseViewController
+
+@end
---
+++ WXSTransition/XibTestViewController.m
@@ -0,0 +1,26 @@
+//
+//  XibTestViewController.m
+//  WXSTransition
+//
+//  Created by AlanWang on 17/1/22.
+//  Copyright © 2017年 王小树. All rights reserved.
+//
+
+#import "XibTestViewController.h"
+
+@interface XibTestViewController ()
+
+@end
+
+@implementation XibTestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
