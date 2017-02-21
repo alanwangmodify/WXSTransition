5541042187cf113611fad38486dc4e3995c63ced
AlanWang <alanwangmodify@126.com>
2017-02-21 17:31:03 +0800

    show navigation

--- WXSTransition/WXSBaseViewController.m
+++ WXSTransition/WXSBaseViewController.m
@@ -22,6 +22,11 @@
     [super viewDidLoad];
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    self.navigationController.navigationBarHidden = NO;
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
