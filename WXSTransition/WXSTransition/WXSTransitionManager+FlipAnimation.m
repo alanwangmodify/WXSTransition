f7c33b673b45f80d624c278997a1980c8863dccc
treewang <treewang@lexinfintech.com>
2017-06-17 16:50:06 +0800

    filp next

--- WXSTransition/WXSTransition/WXSTransitionManager+FlipAnimation.m
+++ WXSTransition/WXSTransition/WXSTransitionManager+FlipAnimation.m
@@ -20,40 +20,84 @@
     CGFloat screenWidth = [UIScreen mainScreen].bounds.size.width;
     CGFloat screenHeight = [UIScreen mainScreen].bounds.size.height;
     
-    CALayer *backLayer = [CALayer layer];
-    backLayer.frame = CGRectMake(0, 0, screenWidth, screenHeight);
-    backLayer.backgroundColor = [UIColor blackColor].CGColor;
-    backLayer.opacity = 0.4;
+
+    UIImage *topImg = [self imageFromView:toView atFrame:CGRectMake(0, 0, screenWidth, screenHeight/2)];
+    UIImageView *topView = [[UIImageView alloc] initWithImage:topImg];
+    [topView setContentMode:UIViewContentModeScaleAspectFill];
+    topView.backgroundColor = [UIColor whiteColor];
+    topView.layer.transform = CATransform3DMakeRotation(M_PI, 1.0, 0.0, 0.0);
     
     
-    UIView *flipView = [[UIView alloc] init];
-    flipView.frame = CGRectMake(screenWidth/4, screenHeight/3, screenWidth/3, screenHeight/3);
-    flipView.center = containView.center;
+    UIImage *fromImgTop = [self imageFromView:fromView atFrame:CGRectMake(0, 0, screenWidth, screenHeight/2)];
+    UIImageView *fromTopView = [[UIImageView alloc] initWithImage:fromImgTop];
     
-    UIImage *topImg = [self imageFromView:toView atFrame:CGRectMake(0, 0, toView.bounds.size.width, toView.bounds.size.height/2)];
-    UIImageView *topView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, flipView.bounds.size.width, flipView.bounds.size.height/2)];
-    topView.image = topImg;
-    
-    
-    UIImage *bottomImg = [self imageFromView:toView atFrame:CGRectMake(0, toView.bounds.size.height/2, toView.bounds.size.width, toView.bounds.size.height/2) ];
-    UIImageView *bottomView = [[UIImageView alloc] initWithFrame:CGRectMake(0, flipView.bounds.size.height/2, flipView.bounds.size.width, flipView.bounds.size.height/2)];
-    bottomView.image = bottomImg;
-    
+    UIImage *fromImgBottom = [self imageFromView:fromView atFrame:CGRectMake(0, screenHeight/2, screenWidth, screenHeight/2)];
+    UIImageView *fromBottomView = [[UIImageView alloc] initWithImage:fromImgBottom];
+    fromBottomView.layer.doubleSided = NO;
     
     //addsubView
-    [containView.layer addSublayer:backLayer];
-    [containView addSubview:fromView];
-    [containView addSubview:flipView];
-    [flipView addSubview:topView];
-    [flipView addSubview:bottomView];
+    [containView addSubview:toView];
+    [containView addSubview:topView];
+    fromTopView.alpha = 0.0;
+    [containView addSubview:fromTopView];
+    [containView addSubview:fromBottomView];
+    fromTopView.alpha = 1.0;
     
-    [transitionContext completeTransition:YES];
-    //            _transformView.upperBackLayer.transform = CATransform3DMakeRotation(-M_PI_2, 1.0, 0.0, 0.0);
-//    UIView *topView = [[UIView alloc] initWithFrame:<#(CGRect)#>]
+    [UIView animateWithDuration:self.animationTime * 1.2 animations:^{
+        fromBottomView.layer.transform = CATransform3DMakeRotation(-M_PI, 1.0, 0.0, 0.0);
+        topView.layer.transform = CATransform3DIdentity;
+    } completion:^(BOOL finished) {
+        [transitionContext completeTransition:YES];
+        [fromTopView removeFromSuperview];
+        [fromBottomView removeFromSuperview];
+        [topView removeFromSuperview];
+    }];
+
     
 }
 - (void)tipFlipBackAnimationContext:(id<UIViewControllerContextTransitioning>)transitionContext {
     
+    UIViewController *fromVC = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
+    UIViewController *toVC = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
+    UIView *containView = [transitionContext containerView];
+    UIView *fromView = fromVC.view;
+    UIView *toView = toVC.view;
+    
+    CGFloat screenWidth = [UIScreen mainScreen].bounds.size.width;
+    CGFloat screenHeight = [UIScreen mainScreen].bounds.size.height;
+    
+    
+    UIImage *topImg = [self imageFromView:toView atFrame:CGRectMake(0, 0, screenWidth, screenHeight/2)];
+    UIImageView *topView = [[UIImageView alloc] initWithImage:topImg];
+    [topView setContentMode:UIViewContentModeScaleAspectFill];
+    topView.backgroundColor = [UIColor whiteColor];
+    topView.layer.transform = CATransform3DMakeRotation(M_PI, 1.0, 0.0, 0.0);
+    
+    
+    UIImage *fromImgTop = [self imageFromView:fromView atFrame:CGRectMake(0, 0, screenWidth, screenHeight/2)];
+    UIImageView *fromTopView = [[UIImageView alloc] initWithImage:fromImgTop];
+    
+    UIImage *fromImgBottom = [self imageFromView:fromView atFrame:CGRectMake(0, screenHeight/2, screenWidth, screenHeight/2)];
+    UIImageView *fromBottomView = [[UIImageView alloc] initWithImage:fromImgBottom];
+    fromBottomView.layer.doubleSided = NO;
+    
+    //addsubView
+    [containView addSubview:toView];
+    [containView addSubview:topView];
+    fromTopView.alpha = 0.0;
+    [containView addSubview:fromTopView];
+    [containView addSubview:fromBottomView];
+    fromTopView.alpha = 1.0;
+    
+    [UIView animateWithDuration:self.animationTime * 1.2 animations:^{
+        fromBottomView.layer.transform = CATransform3DMakeRotation(-M_PI, 1.0, 0.0, 0.0);
+        topView.layer.transform = CATransform3DIdentity;
+    } completion:^(BOOL finished) {
+        [transitionContext completeTransition:YES];
+        [fromTopView removeFromSuperview];
+        [fromBottomView removeFromSuperview];
+        [topView removeFromSuperview];
+    }];
 }
 
 
