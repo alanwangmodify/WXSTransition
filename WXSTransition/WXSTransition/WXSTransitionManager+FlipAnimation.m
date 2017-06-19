cd2c1831da10867109229bea7ad8506664e6a2d1
treewang <treewang@lexinfintech.com>
2017-06-19 17:08:15 +0800

    fix bug

--- WXSTransition/WXSTransition/WXSTransitionManager+FlipAnimation.m
+++ WXSTransition/WXSTransition/WXSTransitionManager+FlipAnimation.m
@@ -10,57 +10,7 @@
 
 @implementation WXSTransitionManager (FlipAnimation)
 - (void)tipFlipToNextAnimationContext:(id<UIViewControllerContextTransitioning>)transitionContext {
-    
-    UIViewController *fromVC = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
-    UIViewController *toVC = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
-    UIView *containView = [transitionContext containerView];
-    UIView *fromView = fromVC.view;
-    UIView *toView = toVC.view;
-    
-    CGFloat screenWidth = [UIScreen mainScreen].bounds.size.width;
-    CGFloat screenHeight = [UIScreen mainScreen].bounds.size.height;
-    
-
-    UIImage *topImg = [self imageFromView:toView atFrame:CGRectMake(0, 0, screenWidth, screenHeight/2)];
-    UIImageView *topView = [[UIImageView alloc] initWithImage:topImg];
-    [topView setContentMode:UIViewContentModeScaleAspectFill];
-    topView.layer.transform = CATransform3DMakeRotation(M_PI, 1.0, 0.0, 0.0);
-    topView.layer.doubleSided = NO;
-    
-    UIImage *fromImgTop = [self imageFromView:fromView atFrame:CGRectMake(0, 0, screenWidth, screenHeight/2)];
-    UIImageView *fromTopView = [[UIImageView alloc] initWithImage:fromImgTop];
-    fromTopView.backgroundColor = [UIColor clearColor];
-    fromTopView.layer.doubleSided = NO;
-    
-    UIImage *fromImgBottom = [self imageFromView:fromView atFrame:CGRectMake(0, screenHeight/2, screenWidth, screenHeight/2)];
-    UIImageView *fromBottomView = [[UIImageView alloc] initWithImage:fromImgBottom];
-    fromBottomView.layer.doubleSided = NO;
-    
-    //addsubView
-   
-    [containView addSubview:toView];
-    [containView addSubview:fromTopView];
-    [containView addSubview:topView];
-    fromTopView.alpha = 0.0;
-    [containView addSubview:fromBottomView];
-    fromTopView.alpha = 1.0;
-    
-    [UIView animateWithDuration:self.animationTime animations:^{
-        fromBottomView.layer.transform = CATransform3DMakeRotation(- M_PI, 1.0, 0.0, 0.0);
-        topView.layer.transform = CATransform3DIdentity;
-    } completion:^(BOOL finished) {
-        [fromTopView removeFromSuperview];
-        [fromBottomView removeFromSuperview];
-        [topView removeFromSuperview];
-        
-        if ([transitionContext transitionWasCancelled]) {
-            [transitionContext completeTransition:NO];
-        }else {
-            [containView bringSubviewToFront:toView];
-            [transitionContext completeTransition:YES];
-        }
-        
-    }];
+    [self tipFlipBackAnimationContext:transitionContext];
 }
 - (void)tipFlipBackAnimationContext:(id<UIViewControllerContextTransitioning>)transitionContext {
     
@@ -89,14 +39,17 @@
     UIImageView *fromBottomView = [[UIImageView alloc] initWithImage:fromImgBottom];
     fromBottomView.layer.doubleSided = NO;
     
+    UIView *flipView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, screenWidth, screenHeight)];
+    flipView.backgroundColor = [UIColor clearColor];
+    [flipView.layer addSublayer:topView.layer];
+    [flipView.layer addSublayer:fromBottomView.layer]; 
+    
     //addsubView
     
     [containView addSubview:toView];
     [containView addSubview:fromTopView];
-    [containView addSubview:topView];
-    fromTopView.alpha = 0.0; //等fromBottomView 遮盖住 fromTopView 再显示fromTopView
-    [containView addSubview:fromBottomView];
-    fromTopView.alpha = 1.0;
+    [containView addSubview:flipView];
+    
     
     [UIView animateWithDuration:self.animationTime animations:^{
         fromBottomView.layer.transform = CATransform3DMakeRotation(- M_PI, 1.0, 0.0, 0.0);
@@ -105,6 +58,7 @@
         [fromTopView removeFromSuperview];
         [fromBottomView removeFromSuperview];
         [topView removeFromSuperview];
+        [flipView removeFromSuperview];
         
         if ([transitionContext transitionWasCancelled]) {
             [transitionContext completeTransition:NO];
@@ -120,6 +74,7 @@
             [fromTopView removeFromSuperview];
             [fromBottomView removeFromSuperview];
             [topView removeFromSuperview];
+            [flipView removeFromSuperview];
         }
     };
     
